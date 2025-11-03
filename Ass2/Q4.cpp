9b0ffb701da4392f9ea742ce52e41d36db0f4762
Adityaraj Singh <singhadityaraj.688@gmail.com>
2025-11-03 13:04:03 +0530

    Added Q4(string operations)

--- Ass2/Q4.cpp
+++ Ass2/Q4.cpp
@@ -25,14 +25,13 @@ string reverse(string s) {
 }
 
 //c
+bool vowel(char c){
+if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
+    return true;
+}
+return false;
+};
 string RemoveVowels(string s) {
-    bool vowel(char c) {
-    if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
-        return true;
-    }
-    return false;
-    };
-
     string ans="";
     for (int i=0;i<s.size();i++) {
         if (!vowel(s[i])) ans+=s[i];
